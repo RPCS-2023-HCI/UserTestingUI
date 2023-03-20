68f1a48dcee092ba66c37bec24d10556611b4802
mauld-cmu <112352959+mauld-cmu@users.noreply.github.com>
2023-03-20 13:15:31 -0400

    Added Grid Formatting

--- diagnostic-ui/src/App.js
+++ diagnostic-ui/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import LineChart from './components/LineChart';
 import Tabs from "./components/ViewTabs/ViewTabs";
 import StartStopButton from './components/StartStopButton/StartStopButton'; 
@@ -8,20 +10,20 @@ function App() {
 
   return (
     <div className='App'>
-      <header className="App-Header">
-        <StartStopButton />
-      </header>
-
-      <div className='Video-Component'>
-        <div className='Video-Toggle'>
-          <Tabs />
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={2}>
+            <StartStopButton />
+          </Grid>
+          <Grid item xs={10}>
+            <Tabs />
+          </Grid>
+        </Grid>
+        <div style={{ width: '50vw', height: '50vh' }}>
+          <LineChart dataType={"Speed"} simulationId={"ExampleGraphTest"} />
+          <LineChart dataType={"EngineTemperature"} simulationId={"ExampleGraphTest"} />
         </div>
-      </div>
-
-      <div style={{ width: '50vw', height: '50vh' }}>
-        <LineChart dataType={"Speed"} simulationId={"ExampleGraphTest"} />
-        <LineChart dataType={"EngineTemperature"} simulationId={"ExampleGraphTest"} />
-      </div>
+      </Container>
     </div>
   );
 }
