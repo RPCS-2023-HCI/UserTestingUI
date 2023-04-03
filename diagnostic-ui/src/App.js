65d29f984f2ec1da8b6f155e9d7f435cab75e71f
mauld-cmu <112352959+mauld-cmu@users.noreply.github.com>
2023-04-03 15:07:13 -0400

    Removed some redundant/no longer needed code - Added note to LineChart which seems busted

--- diagnostic-ui/src/App.js
+++ diagnostic-ui/src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
 import React from 'react';
-import LineChart from './components/LineChart';
-import Tabs from "./components/ViewTabs/ViewTabs";
-import StartStopButton from './components/StartStopButton/StartStopButton'; 
-import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
 
 function App() {
