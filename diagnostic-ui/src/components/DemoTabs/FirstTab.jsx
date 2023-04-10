bbfb347d875cd01a91985d08cb78cca355eaed72
atirupat <122955023+atirupat@users.noreply.github.com>
2023-04-10 16:58:38 -0400

    Trying to display webcam stream into website

--- diagnostic-ui/src/components/DemoTabs/FirstTab.jsx
+++ diagnostic-ui/src/components/DemoTabs/FirstTab.jsx
@@ -4,16 +4,21 @@ import React from "react";
 const FirstTab = () => {
   return (
     <div className="First-Tab">
-      <iframe width="560" 
+      {/* <iframe width="560" 
         height="315" 
         src="https://www.youtube.com/embed/KLOceDeegVs" 
         title="YouTube video player" 
         frameborder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
         allowfullscreen>
-      </iframe>
+      </iframe> */}
+
+    {/* <embed type="video/webm" src="udp://@0.0.0.0:5000" width="400" height="300">  */}
+      <video src="rtsp://@0.0.0.0:5000" brightsign-properties="StreamTimeout:0;StreamLowLatency:0;"> 
+      </video>
     </div>
   );
 };
+
 export default FirstTab;
 
