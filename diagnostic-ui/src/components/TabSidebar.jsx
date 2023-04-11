9b206ce695349b5af5fe64909766d8ae8ee66771
mauld-cmu <112352959+mauld-cmu@users.noreply.github.com>
2023-04-11 10:59:59 -0400

    Gave Tabs Icons - Removed excess tabs from example

--- diagnostic-ui/src/components/TabSidebar.jsx
+++ diagnostic-ui/src/components/TabSidebar.jsx
@@ -4,6 +4,8 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import FolderIcon from '@mui/icons-material/Folder';
+import DirectionsCarFilledIcon from '@mui/icons-material/DirectionsCarFilled';
 
 function TabSidebar(props) {
   const { children, value, index, ...other } = props;
@@ -54,37 +56,18 @@ export default function VerticalTabs() {
         variant="scrollable"
         value={value}
         onChange={handleChange}
-        aria-label="Vertical tabs example"
+        aria-label="Vertical Tabs"
         sx={{ borderRight: 1, borderColor: 'divider' }}
       >
-        <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-        <Tab label="Item Four" {...a11yProps(3)} />
-        <Tab label="Item Five" {...a11yProps(4)} />
-        <Tab label="Item Six" {...a11yProps(5)} />
-        <Tab label="Item Seven" {...a11yProps(6)} />
+        <Tab icon={<DirectionsCarFilledIcon />} aria-label="car"{...a11yProps(0)} />
+        <Tab icon={<FolderIcon />} aria-label="folder" {...a11yProps(1)} />
+
       </Tabs>
       <TabSidebar value={value} index={0}>
-        Item One
+        Vehicle Status
       </TabSidebar>
       <TabSidebar value={value} index={1}>
-        Item Two
-      </TabSidebar>
-      <TabSidebar value={value} index={2}>
-        Item Three
-      </TabSidebar>
-      <TabSidebar value={value} index={3}>
-        Item Four
-      </TabSidebar>
-      <TabSidebar value={value} index={4}>
-        Item Five
-      </TabSidebar>
-      <TabSidebar value={value} index={5}>
-        Item Six
-      </TabSidebar>
-      <TabSidebar value={value} index={6}>
-        Item Seven
+        Files
       </TabSidebar>
     </Box>
   );
