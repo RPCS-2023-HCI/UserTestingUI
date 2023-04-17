3dbf3103e133fd28cb4f31dc7bd420fa645688e3
atirupat <122955023+atirupat@users.noreply.github.com>
2023-04-17 16:54:21 -0400

    Jitsi Meet Video Embedded

--- diagnostic-ui/src/components/VideoComponent.jsx
+++ diagnostic-ui/src/components/VideoComponent.jsx
@@ -1,17 +1,10 @@
+import './VideoComponent.css'
 import React from 'react';
 
 function VideoComponent() {
   return (
     <div className="First-Tab">
-      <iframe width="560"
-        height="315"
-        src="https://www.youtube.com/embed/KLOceDeegVs"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen>
-      </iframe>
-      {/* <embed type="video/webm" src="udp://@0.0.0.0:5000" width="400" height="300">  */}
+      <iframe allow="camera; microphone; fullscreen; display-capture; autoplay" src="https://meet.jit.si/RPCS-test"></iframe>
     </div>
   );
 }
