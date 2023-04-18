f5f682da39f97ce4b63ca43b8d620bdc7224e32a
jzhang999 <56798336+jzhang999@users.noreply.github.com>
2023-04-18 16:45:11 -0400

    add abnormal point onto the graph

--- diagnostic-ui/src/components/LineChart.js
+++ diagnostic-ui/src/components/LineChart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Line } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 import { registerables } from 'chart.js';
@@ -7,35 +7,75 @@ import { Container } from 'react-bootstrap';
 Chart.register(...registerables);
 
 function LineChart(props) {
+  function getLabels() {
+    let name = props.dataType;
+    return props.data["Object"]["Item"][name]["time"];
+  }
 
-    function getLabels() {
-        let name = props.dataType;
-        return props.data["Object"]["Item"][name]["time"];
-    }
+  function getDataset() {
+    let name = props.dataType;
+    return [
+      {
+        id: 2, 
+        yAxisID: 'y2',
+        label: 'Abnormal',
+        data: [{ x: 0, y: 0 }],
+        borderColor: 'rgba(208, 49, 45, 0.7)', 
+        backgroundColor: 'rgba(208, 49, 45, 0.7)',
+        pointBorderColor: 'rgba(208, 49, 45, 0.7)', 
+        pointBackgroundColor: 'rgba(208, 49, 45, 0.7)', 
+        pointRadius: 3,
+      },
+      {
+        id: 1, 
+        yAxisID: 'y',
+        label: props.dataType,
+        data: props.data["Object"]["Item"][name]["value"],
+        borderColor: 'rgba(85, 170, 255, 0.7)', 
+        backgroundColor: 'rgba(85, 170, 255, 0.4)', 
+        pointBorderColor: 'rgba(85, 170, 255, 0.7)',
+        pointBackgroundColor: 'rgba(85, 170, 255, 0.7)',
+        pointRadius: 3,
+      },
+    ];
+  }
 
-    function getDataset() {
-        let name = props.dataType;
-        return [
-            {
-                id: 1,
-                label: props.dataType,
-                data: props.data["Object"]["Item"][name]["value"],
+  return (
+    <Container>
+      <h4 style={{ textAlign: 'center', color: 'grey' }}>{props.title}</h4>
+      <Line
+        datasetIdKey="id"
+        data={{
+          labels: getLabels(),
+          datasets: getDataset(),
+        }}
+        options={{
+          scales: {
+            y: {
+              type: 'linear',
+              display: true,
+              position: 'left',
+              id: 'y',
             },
-        ];
-    }
-
-    return (
-        <Container>
-            <h4 style={{textAlign: 'center', color: 'grey'}}>{props.title}</h4>
-            <Line
-            datasetIdKey='id'
-            data={{
-                labels: getLabels(),
-                datasets: getDataset(),
-            }}
-            />
-        </Container>
-    );
+            y2: {
+              type: 'linear',
+              display: false,
+              position: 'right',
+              id: 'y2',
+              grid: {
+                drawOnChartArea: false,
+              },
+            },
+          },
+          plugins: {
+            legend: {
+              display: true,
+            },
+          },
+        }}
+      />
+    </Container>
+  );
 }
 
 export default LineChart;
