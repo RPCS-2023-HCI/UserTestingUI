df1e5e719c114470392c82af62a81b4eaf085bc6
jzhang999 <56798336+jzhang999@users.noreply.github.com>
2023-04-18 14:29:44 -0400

    fix edge cases of the path drwaing and enlarge it

--- diagnostic-ui/src/components/GPSMap.js
+++ diagnostic-ui/src/components/GPSMap.js
@@ -16,7 +16,7 @@ class GPSMap extends Component {
       requestAnimationFrame(this.animatePoints);
     }
   }
-    
+
   componentDidUpdate(prevProps) {
     if (prevProps.isDrawing !== this.props.isDrawing) {
       if (this.props.isDrawing) {
@@ -27,39 +27,78 @@ class GPSMap extends Component {
     }
   }
 
+  getLatLongBounds(points) {
+    let minLat = Infinity;
+    let maxLat = -Infinity;
+    let minLong = Infinity;
+    let maxLong = -Infinity;
+
+    points.forEach(point => {
+      const data = point.split(',');
+      const lat = parseFloat(data[2]);
+      const long = parseFloat(data[4]);
+      minLat = Math.min(minLat, lat);
+      maxLat = Math.max(maxLat, lat);
+      minLong = Math.min(minLong, long);
+      maxLong = Math.max(maxLong, long);
+    });
+
+    return { minLat, maxLat, minLong, maxLong };
+  }
+
   animatePoints = () => {
+    // handle when canvas is not ready
+    if (!this.mapRef.current) {
+      this.requestID = requestAnimationFrame(this.animatePoints);
+      return;
+    }
+  
     const { currentIndex, points } = this.state;
     const ctx = this.mapRef.current.getContext('2d');
-    const scale = 4000;
+    const scale = 10000;
+  
+    const { minLat, maxLat, minLong, maxLong } = this.getLatLongBounds(points);
+    const latCenter = (minLat + maxLat) / 2;
+    const longCenter = (minLong + maxLong) / 2;
+  
     const data = points[currentIndex].split(',');
     const lat = parseFloat(data[2]);
     const lng = parseFloat(data[4]);
     const centerX = ctx.canvas.width / 2;
     const centerY = ctx.canvas.height / 2;
-    const x = (centerX + lng * scale) % ctx.canvas.width;
-    const y = (centerY + lat * scale) % ctx.canvas.height;
+  
+    const xOffset = centerX - longCenter * scale;
+    const yOffset = centerY - latCenter * scale;
+  
     const rectSize = 10;
-    const rectX = (centerX + lng * scale) % ctx.canvas.width;
-    const rectY = (centerY + lat * scale) % ctx.canvas.height;
+    const rectX = (lng * scale) + xOffset;
+    const rectY = (lat * scale) + yOffset;
+  
+    // Clear previous rectangle
+    if (currentIndex > 0) {
+      const prevData = points[currentIndex - 1].split(',');
+      const prevLat = parseFloat(prevData[2]);
+      const prevLng = parseFloat(prevData[4]);
+      const prevRectX = prevLng * scale + xOffset;
+      const prevRectY = prevLat * scale + yOffset;
+      ctx.clearRect(
+        prevRectX - rectSize / 2,
+        prevRectY - rectSize / 2,
+        rectSize,
+        rectSize
+      );
+    }
   
     // Draw rectangle
     ctx.fillStyle = 'green';
     ctx.fillRect(rectX - rectSize / 2, rectY - rectSize / 2, rectSize, rectSize);
   
-    // Clear previous rectangle
-    const prevData = points[Math.max(currentIndex - 1, 0)].split(',');
-    const prevLat = parseFloat(prevData[2]);
-    const prevLng = parseFloat(prevData[4]);
-    const prevX = (centerX + prevLng * scale) % ctx.canvas.width;
-    const prevY = (centerY + prevLat * scale) % ctx.canvas.height;
-    ctx.clearRect(prevX - rectSize / 2, prevY - rectSize / 2, rectSize, rectSize);
-  
     // Draw black lines
     if (currentIndex === 0) {
       ctx.beginPath();
-      ctx.moveTo(x, y);
+      ctx.moveTo(rectX, rectY);
     } else {
-      ctx.lineTo(x, y);
+      ctx.lineTo(rectX, rectY);
       ctx.stroke();
     }
   
@@ -69,7 +108,7 @@ class GPSMap extends Component {
         if (this.props.isDrawing) {
           this.requestID = requestAnimationFrame(this.animatePoints);
         }
-      }, 1000); // Change this value to adjust the delay between points
+      }, 500); // Change this value to adjust the delay between points
     }
   };
 
