e19c33f3192fe5e9f30791b8cdcc0724dbdf23bb
elisewangxinyi <112357153+elisewangxinyi@users.noreply.github.com>
2023-04-26 13:27:35 -0400

    updated track sensor units

--- diagnostic-ui/src/components/VehicleTab/EnvSubTab.jsx
+++ diagnostic-ui/src/components/VehicleTab/EnvSubTab.jsx
@@ -20,9 +20,9 @@ function EnvSubTab() {
         >
             <Typography variant="h6">Track Sensor Readings</Typography>
             <br/>
-            Elapsed Time: {elpTime} s <br/>
-            Distance to Obstacle 1: {distOb1} in <br/>
-            Distance to Obstacle 2: {distOb2} in <br/>
+            Elapsed Time: {elpTime} milliseconds <br/>
+            Distance to Obstacle 1: {distOb1} cm <br/>
+            Distance to Obstacle 2: {distOb2} cm <br/>
         </Paper>
         <Paper
           sx={{
